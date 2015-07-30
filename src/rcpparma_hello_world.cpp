6ad0e7b08a0e67f852e68cc577d5180ce69aeab6
Jan Tilly <jantilly@gmail.com>
2015-07-30 12:54:03 -0400

    added documentation

--- src/rcpparma_hello_world.cpp
+++ src/rcpparma_hello_world.cpp
@@ -8,32 +8,44 @@
 //
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// simple example of creating two matrices and
-// returning the result of an operatioon on them
-//
-// via the exports attribute we tell Rcpp to make this function
-// available from R
-//
+
+//' Hello World
+//'
+//' simple example of creating two matrices and returning the result of an
+//' operation on them
+//'
+//' @return matrix
 // [[Rcpp::export]]
 arma::mat rcpparma_hello_world() {
     arma::mat m1 = arma::eye<arma::mat>(3, 3);
     arma::mat m2 = arma::eye<arma::mat>(3, 3);
-	                     
+
     return m1 + 3 * (m1 + m2);
 }
 
 
-// another simple example: outer product of a vector, 
-// returning a matrix
-//
+//' Outer product
+//'
+//' another simple example: outer product of a vector,
+//' returning a matrix
+//'
+//' @param x column vector
+//' @return matrix with the outer product
+//'
 // [[Rcpp::export]]
 arma::mat rcpparma_outerproduct(const arma::colvec & x) {
     arma::mat m = x * x.t();
     return m;
 }
 
-// and the inner product returns a scalar
-//
+//' Inner product
+//'
+//' another simple example: inner product of a vector,
+//' returning a scalar
+//'
+//' @param x column vector
+//' @return scalar with the inner product
+//'
 // [[Rcpp::export]]
 double rcpparma_innerproduct(const arma::colvec & x) {
     double v = arma::as_scalar(x.t() * x);
@@ -41,8 +53,13 @@ double rcpparma_innerproduct(const arma::colvec & x) {
 }
 
 
-// and we can use Rcpp::List to return both at the same time
-//
+//' Return both the inner and outer product
+//'
+//' Return a list with both the inner and outer product
+//'
+//' @param x column vector
+//' @return list with both the inner and outer product
+//'
 // [[Rcpp::export]]
 Rcpp::List rcpparma_bothproducts(const arma::colvec & x) {
     arma::mat op = x * x.t();
